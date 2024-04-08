b253769a1625bd7adbaba38ed365b5162e44c8b3
Carlos Garcia <cagarciagarcia@gmail.com>
2024-04-08 13:35:10 +0200

    completado lista productos y cesta productos. pendiente actualizar stock al cambiar categoria.

--- src/app/controller/procesar-pedido/procesar-pedido.component.ts
+++ src/app/controller/procesar-pedido/procesar-pedido.component.ts
@@ -2,6 +2,7 @@ import { ProcesarPedidoService } from './../../service/procesar-pedido.service';
 import { Component, OnInit } from '@angular/core';
 import { Categoria } from '../../model/Categoria';
 import { Producto } from '../../model/Producto';
+import { CestaItem } from '../../model/CestaItem';
 
 @Component({
   selector: 'app-procesar-pedido',
@@ -12,11 +13,13 @@ export class ProcesarPedidoComponent implements OnInit{
   categorias:Categoria[];
   productos:Producto[];
   idCategoriaSelec:number;
+  cesta:CestaItem[];
   constructor(private procesarPedidoService:ProcesarPedidoService){
 
   }
   ngOnInit(): void {
     this.procesarPedidoService.categorias().subscribe(c=>this.categorias=c);
+    this.cesta=[];
     console.log("onInit1");
   }
 
@@ -24,6 +27,29 @@ export class ProcesarPedidoComponent implements OnInit{
     console.log("productosCategoria1");
     this.procesarPedidoService.productosCategorias(this.idCategoriaSelec).subscribe(p=>this.productos=p);
     console.log("productosCategoria2");
+  }
+
+  agregarProductoCesta(producto:Producto){
+    if (producto.unidades <=producto.stock){
+      let item= new CestaItem();
+      item.producto=producto;
+      item.unidades=producto.unidades;
+      //Actualizar Stock:
+      producto.stock=producto.stock-producto.unidades;
+      this.cesta.push(item);
+    }else{
+      alert("No hay suficiente stock");
+    }
+  }
+
+  eliminarProductoCesta(pos:number){
+    let item=this.cesta[pos];
+    this.cesta.splice(pos,1);
+    //Actualizar Stock: primero localizamos el producto
+    let producto=this.productos.find(p=>p.idProducto==item.producto.idProducto);
+    //ahora recuperamos las unidades al stock
+    producto.stock= producto.stock+item.unidades;//lo que hace es concatenarlos asi que le ponemos delante el tipo Number(valor).
+    producto.stock= Number(producto.stock)+Number(item.unidades);
   }
 
 
