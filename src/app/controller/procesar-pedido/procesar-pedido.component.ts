947d94fd92ed1b712c839e33fdc3bab4596e761e
Carlos Garcia <cagarciagarcia@gmail.com>
2024-04-09 13:38:34 +0200

    empezamos a meter los servicios de procesar pedidos

--- src/app/controller/procesar-pedido/procesar-pedido.component.ts
+++ src/app/controller/procesar-pedido/procesar-pedido.component.ts
@@ -25,8 +25,24 @@ export class ProcesarPedidoComponent implements OnInit{
 
   productosCategoria(){
     console.log("productosCategoria1");
-    this.procesarPedidoService.productosCategorias(this.idCategoriaSelec).subscribe(p=>this.productos=p);
+    this.procesarPedidoService.productosCategorias(this.idCategoriaSelec).subscribe(p=>{
+      this.productos=p;
+      this.actualizarStocks();//metemos esta llamada para solucionar el problema de que al cambiar de categoria, el stock no coincide.
+      });
     console.log("productosCategoria2");
+  }
+
+  actualizarStocks(){
+    this.productos.forEach(p=>{
+      this.cesta.forEach(c=>{
+        //para cada producto de la categoria seleccionada
+        //recorremos la cesta de la compra y si encontramos ese producto
+        //en la cesta, debemos actualizar su stock
+        if(p.idProducto==c.producto.idProducto){
+          p.stock=p.stock-c.unidades;
+        }
+      })
+    })
   }
 
   agregarProductoCesta(producto:Producto){
@@ -48,9 +64,13 @@ export class ProcesarPedidoComponent implements OnInit{
     //Actualizar Stock: primero localizamos el producto
     let producto=this.productos.find(p=>p.idProducto==item.producto.idProducto);
     //ahora recuperamos las unidades al stock
-    producto.stock= producto.stock+item.unidades;//lo que hace es concatenarlos asi que le ponemos delante el tipo Number(valor).
+    //producto.stock= producto.stock+item.unidades;//lo que hace es concatenarlos asi que le ponemos delante el tipo Number(valor).
     producto.stock= Number(producto.stock)+Number(item.unidades);
   }
 
+
+  procesarPedido(){
+    this.procesarPedidoService.enviarPedido(this.cesta, "user1"); //ponemos este usuario temporalmente hasta que conectemos todo, ya que no tenemos ahora el servicio de usuarios. luego sera (usuario: sting)
+  }
 
 }
--- src/app/service/procesar-pedido.service.ts
+++ src/app/service/procesar-pedido.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CestaItem } from '../model/CestaItem';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,13 @@ export class ProcesarPedidoService {
     let params=new HttpParams();
     params=params.append("idCategoria", idCategoria)
     return this.http.get(url,{"params":params});
+  }
+
+  enviarPedido(cesta:CestaItem[], usuario:string):Observable<any>{
+    let url="http://localhost:8082/pedido";
+    let params=new HttpParams();
+    params=params.append("usuario", usuario);
+    return this.http.post(url,cesta,{"params":params});
   }
 
 }
